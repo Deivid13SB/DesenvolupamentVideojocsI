87c045721b747cd7fedf814fede38337b0e2ba9f
Mario3212332123 <mariotorrents@gmail.com>
2025-01-23 22:16:46 +0100

    UPDATE TILESET

---
+++ Assets/Maps/tmw_desert_spacing.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="tmw_desert_spacing" tilewidth="32" tileheight="32" spacing="2" tilecount="400" columns="20">
+ <image source="tmw_desert_spacing.png" width="678" height="678"/>
+</tileset>
